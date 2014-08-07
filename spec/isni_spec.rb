db9312d7816b6f68ad21366b6ce07e668c3573a2
James Healy <james@yob.id.au>
2014-08-07 17:28:20 +1000

    add another valid ISNI to specs for extra confidence

--- spec/isni_spec.rb
+++ spec/isni_spec.rb
@@ -5,6 +5,7 @@ describe "The ISNI class" do
     it "should identify a valid ISNI" do
       expect(ISNI.new("0000-0002-9534-656X").valid?).to eq true
       expect(ISNI.new("000000029534656X").valid?).to eq true
+      expect(ISNI.new("0000000122822548").valid?).to eq true
     end
   end
 
@@ -12,6 +13,7 @@ describe "The ISNI class" do
     it "should identify a valid ISNI" do
       expect(ISNI.valid?("0000-0002-9534-656X")).to eq true
       expect(ISNI.valid?("000000029534656X")).to eq true
+      expect(ISNI.valid?("0000000122822548")).to eq true
     end
 
     it "should identify an invalid ISNI" do
@@ -28,6 +30,7 @@ describe "The ISNI class" do
   describe "#complete class method" do
     it "should calculate a ISNI check digit correctly" do
       expect(ISNI.complete("000000029534656")).to eq("000000029534656X")
+      expect(ISNI.complete("000000012282254")).to eq("0000000122822548")
       expect(ISNI.complete("0000-0002-9534-656")).to eq("000000029534656X")
     end
   end
@@ -35,6 +38,7 @@ describe "The ISNI class" do
   describe "#to_s" do
     it "should hyphen a ISNI correctly" do
       expect(ISNI.new("000000029534656X").to_s).to eq("0000-0002-9534-656X")
+      expect(ISNI.new("0000000122822548").to_s).to eq("0000-0001-2282-2548")
     end
   end
 end
