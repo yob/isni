6f405ff20189c43c9ea692ccb50853711bed5a5b
James Healy <james@yob.id.au>
2014-08-07 17:24:08 +1000

    update specs to rspec 3 syntax

--- spec/isni_spec.rb
+++ spec/isni_spec.rb
@@ -3,38 +3,38 @@ require 'isni'
 describe "The ISNI class" do
   describe "#valid? instance method" do
     it "should identify a valid ISNI" do
-      ISNI.new("0000-0002-9534-656X").valid?.should eq true
-      ISNI.new("000000029534656X").valid?.should eq true
+      expect(ISNI.new("0000-0002-9534-656X").valid?).to eq true
+      expect(ISNI.new("000000029534656X").valid?).to eq true
     end
   end
 
   describe "#valid? class method" do
     it "should identify a valid ISNI" do
-      ISNI.valid?("0000-0002-9534-656X").should eq true
-      ISNI.valid?("000000029534656X").should eq true
+      expect(ISNI.valid?("0000-0002-9534-656X")).to eq true
+      expect(ISNI.valid?("000000029534656X")).to eq true
     end
 
     it "should identify an invalid ISNI" do
-      ISNI.valid?(nil).should       eq false
-      ISNI.valid?("902865").should  eq false
-      ISNI.valid?(Array).should     eq false
-      ISNI.valid?(Array.new).should eq false
-      ISNI.valid?("0000-0002-9534-6569").should  eq false
-      ISNI.valid?("0000000295346569").should   eq false
-      ISNI.valid?("000000029534656").should   eq false
+      expect(ISNI.valid?(nil)).to       eq false
+      expect(ISNI.valid?("902865")).to  eq false
+      expect(ISNI.valid?(Array)).to     eq false
+      expect(ISNI.valid?(Array.new)).to eq false
+      expect(ISNI.valid?("0000-0002-9534-6569")).to eq false
+      expect(ISNI.valid?("0000000295346569")).to    eq false
+      expect(ISNI.valid?("000000029534656")).to     eq false
     end
   end
 
   describe "#complete class method" do
     it "should calculate a ISNI check digit correctly" do
-      ISNI.complete("000000029534656").should eql("000000029534656X")
-      ISNI.complete("0000-0002-9534-656").should eql("000000029534656X")
+      expect(ISNI.complete("000000029534656")).to eq("000000029534656X")
+      expect(ISNI.complete("0000-0002-9534-656")).to eq("000000029534656X")
     end
   end
 
   describe "#to_s" do
     it "should hyphen a ISNI correctly" do
-      ISNI.new("000000029534656X").to_s.should eql("0000-0002-9534-656X")
+      expect(ISNI.new("000000029534656X").to_s).to eq("0000-0002-9534-656X")
     end
   end
 end
